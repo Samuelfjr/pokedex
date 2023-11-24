534d821195e3551136c2f0dc005d5690135db9f8
Samuelfjr <samucaftww@gmail.com>
2023-11-24 09:23:52 -0300

    feat: adiciona componente navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav>
+        <div>
+          <img
+            alt="pokeapi-logo"
+            src="https://raw.githubusercontent.com/PokeAPI/media/master/logo/pokeapi_256.png"
+            className="navbar-image"
+          />
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
