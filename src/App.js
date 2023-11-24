74b038073a62d45d9ad9fe5c4a0e12e133a0f962
Samuelfjr <samucaftww@gmail.com>
2023-11-24 11:43:30 -0300

    chore: pega todos os pokemons usando useEffect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Searchbar from "./components/Searchbar";
 import Pokedex from "./components/Pokedex";
+import { getPokemons } from "./api";
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [pokemons, setPokemons] = useState([]);
+
+  const fetchPokemons = async () => {
+    setLoading(true);
+    const result = await getPokemons();
+    setPokemons(result);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    console.log("carregou");
+    fetchPokemons();
+  });
+
   return (
     <div>
       <Navbar />
