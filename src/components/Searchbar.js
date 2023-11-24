602c34eb3392a8adb0eb13761f0d76643876579d
Samuelfjr <samucaftww@gmail.com>
2023-11-24 09:38:30 -0300

    feat: adiciona componente searchbar

---
+++ src/components/Searchbar.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Searchbar = () => {
+  let search = "CHARIZARD";
+  const onChangeHandler = (e) => {
+    console.log("pokemon: ", e.target.value);
+    search = e.target.value;
+  };
+  return (
+    <div className="searchbar-container">
+      <div className="searchbar">
+        <input placeholder="Buscar pokemon" onChange={onChangeHandler} />
+        {search}
+      </div>
+    </div>
+  );
+};
+
+export default Searchbar;
