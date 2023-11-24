53a918b0caad6956e43a161e0d8a93ede6a6fc81
Samuelfjr <samucaftww@gmail.com>
2023-11-24 18:37:34 -0300

    refactor: exclui tratamento de erro

--- src/api.js
+++ src/api.js
@@ -1,28 +1,15 @@
 export const searchPokemon = async (pokemon) => {
-  try {
-    let url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
-    const response = await fetch(url);
-    return await response.json();
-  } catch (error) {
-    console.log("error: ", error);
-  }
+  let url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
+  const response = await fetch(url);
+  return await response.json();
 };
-export const getPokemons = async (limit = 50, offset = 0) => {
-  try {
-    let url = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`;
-    const response = await fetch(url);
-    return await response.json();
-  } catch (error) {
-    console.log("error: ", error);
-  }
+export const getPokemons = async (limit = 300, offset = 0) => {
+  let url = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`;
+  const response = await fetch(url);
+  return await response.json();
 };
 
 export const getPokemonData = async (url) => {
-  try {
-    const response = await fetch(url);
-    return await response.json();
-  } catch (error) {
-    console.log("error: ", error);
-  }
+  const response = await fetch(url);
+  return await response.json();
 };
-
