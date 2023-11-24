e8b076c800e671a31677744655d1e2c7921f1358
Samuelfjr <samucaftww@gmail.com>
2023-11-24 09:29:08 -0300

    chore: constante logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,13 @@
 import React from "react";
 
 const Navbar = () => {
+  const logoImg =
+    "https://raw.githubusercontent.com/PokeAPI/media/master/logo/pokeapi_256.png";
   return (
     <div>
       <nav>
         <div>
-          <img
-            alt="pokeapi-logo"
-            src="https://raw.githubusercontent.com/PokeAPI/media/master/logo/pokeapi_256.png"
-            className="navbar-image"
-          />
+          <img alt="pokeapi-logo" src={logoImg} className="navbar-image" />
         </div>
       </nav>
     </div>
